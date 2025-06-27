d0b3ec856909ede53f3de9c062fba8b04aa87bc6
jankyaf <scott.samaroo@gmail.com>
2025-06-26 21:29:33 -0400

    considering not using SSR

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,3 +5,25 @@
 <template>
   <HelloWorld />
 </template>
+
+<!-- **consider not using SSR** -->
+
+
+<!-- -------------------------------------------------------------------------- -->
+<!--            CONTENT MODULE INSTALLATION TEST (NOT WORKING)                  -->
+<!-- -------------------------------------------------------------------------- -->
+<!--
+<script setup lang="ts">
+const { data: home } = await useAsyncData(() => queryCollection('content').path('/').first())
+
+useSeoMeta({
+  title: home.value?.title,
+  description: home.value?.description
+})
+</script>
+
+<template>
+  <ContentRenderer v-if="home" :value="home" />
+  <div v-else>Home not found</div>
+</template>
+-->
